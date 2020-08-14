772ba59d41a9ffe087cc118e1a0bec1fd1819773
lpgarcia18 <lpgarcia18@gmail.com>
2020-08-14 18:26:50 +0000

    Modelo SEIRD ajustado. Falta ajustar os parâmetros de tempo

--- nowcasting_e_Rt_covid_19.R
+++ nowcasting_e_Rt_covid_19.R
@@ -9,6 +9,7 @@ library(readr)
 library(tidyverse)
 library(forecast)
 library(EpiEstim)
+library(EpiModel)
 library(foreign)
 #devtools::install_github("tidyverse/googlesheets4")
 library(googlesheets4)
@@ -20,8 +21,10 @@ library(RcppRoll)
 # Nowcasting --------------------------------------------------------------------
 #Utiliza-se os dados de casos confirmados do estado de SC 
 #Disponível em: http://dados.sc.gov.br/dataset/casos-19-dados-anonimizados-de-casos-confirmados/resource/76d6dfe8-7fe9-45c1-95f4-cab971803d49
-casos <- read_delim("base/boavista_covid_dados_abertos.csv", 
+base <- read_delim("base/boavista_covid_dados_abertos.csv", 
 					   ";", escape_double = FALSE, trim_ws = TRUE)
+
+casos <- base
 
 casos <- data.frame(INICIO_SINTOMAS = casos$data_inicio_sintomas, 
 		    CASOS = rep.int(1,nrow(casos)))
@@ -115,6 +118,39 @@ ggplot(casos_arima, aes(DATA, CASOS, group = 1))+
 #################################################
 #Estimativa do número de óbitos
 #################################################
+obitos <- base
+
+obitos <- data.frame(DATA = base$data_obito, 
+		    OBITOS = ifelse(obitos$obito == 'SIM', 1, 0))
+
+obitos <- obitos %>% 
+	  group_by(DATA) %>%
+	  summarise(OBITOS = sum(OBITOS, na.rm = T))
+
+obitos$DATA <- as.Date(obitos$DATA, format = "%d/%m/%Y")
+obitos <- na.omit(obitos)
+
+#Recortando datas em que o paciente foi contaminado ao óbito
+contaminacao_ao_obito <- 17
+
+obitos <- obitos[order(obitos$DATA),]
+obitos_trunc <- subset(obitos, obitos$DATA <
+		      	(tail(obitos$DATA,1) - contaminacao_ao_obito))
+
+
+#realizando as projeções do período excluído, para ajustar os artefatos
+obitos_proj <- forecast(auto.arima(obitos_trunc$OBITOS),
+		       h=(nrow(obitos)-nrow(obitos_trunc)+1))$mean[1:(nrow(obitos)-nrow(obitos_trunc))] %>% 
+	as.data.frame()
+
+obitos_proj <- data.frame(obitos = obitos_proj,
+			 DATA = c(max(as.Date(obitos_trunc$DATA)+1):(max(as.Date(obitos_trunc$DATA))+(nrow(obitos)-nrow(obitos_trunc))))) 
+
+names(obitos_proj) <- c("OBITOS", "DATA")
+obitos_proj$DATA <- as.Date(obitos_proj$DATA, origin = "1970-01-01")
+obitos_arima <- rbind(obitos_trunc, obitos_proj) %>% as.data.frame()
+names(obitos_arima) <- c("OBITOS", "DATA")
+
 
 #################################################
 #Estimativa do número de expostos
@@ -127,18 +163,8 @@ ggplot(casos_arima, aes(DATA, CASOS, group = 1))+
 #utilizou-se o modelo de suavização esponencial ou ARIMA, com o menor erro quadrado.
 expostos <- casos_arima
 expostos$EXPOSTOS <- roll_sum(expostos$CASOS,2, fill = 0, align = "right") #menos de 3 dias do início dos sintomas
-expostos_proj <- forecast(auto.arima(expostos$EXPOSTOS),
-			  h=((Sys.Date()-1)-max(as.Date(expostos$DATA))))$mean[1:((Sys.Date()-1)-max(as.Date(expostos$DATA)))] %>%
-	as.data.frame()
-expostos_proj$CASOS_NOVOS <- forecast(auto.arima(expostos$CASOS),
-				      h=((Sys.Date()-1)-max(as.Date(expostos$DATA))))$mean[1:((Sys.Date()-1)-max(as.Date(expostos$DATA)))]
-
-expostos_proj$DATA <- c((max(as.Date(expostos$DATA))+1):(Sys.Date()-1)) %>% as.Date(origin = "1970-01-01") %>%
-	as.character()
-names(expostos_proj) <- c("EXPOSTOS", "CASOS", "DATA")
-expostos <- rbind(expostos, expostos_proj) %>% as.data.frame()
-names(expostos)[1] <- "CASOS_NOVOS"
-
+names(obitos_arima) <- c("OBITOS", "DATA")
+expostos$CASOS <- NULL
 
 #################################################
 #Estimativa do número de infectantes
@@ -150,31 +176,21 @@ names(expostos)[1] <- "CASOS_NOVOS"
 #Esta pesquisa adotou, então, como período infectante aquele entre dois dias antes e 8 dias após o início dos sintomas.
 #Para a estimativa dos casos truncados, utilizou-se o modelo de suavização esponencial
 #ou ARIMA, com o menor erro quadrado
-infectantes <- covid
-infectantes$DATA <- (infectantes$INICIO_SINTOMAS - 3) %>% as.character()
-infectantes$INICIO_SINTOMAS <- NULL
+infectantes <- casos_arima
 infectantes$INFECTANTES <- roll_sum(infectantes$CASOS,11, fill = 0, align = "right") 
-infectantes <- infectantes %>% dplyr::select(DATA, INFECTANTES)
-infectantes_proj <- forecast(auto.arima(infectantes$INFECTANTES),
-			     h=((Sys.Date()-1)-max(as.Date(infectantes$DATA))))$mean[1:((Sys.Date()-1)-max(as.Date(infectantes$DATA)))] %>%
-	as.data.frame()
-infectantes_proj$DATA <- c((max(as.Date(infectantes$DATA))+1):(Sys.Date()-1)) %>% as.Date(origin = "1970-01-01") %>%
-	as.character()
-names(infectantes_proj) <- c("INFECTANTES", "DATA")
-infectantes <- rbind(infectantes, infectantes_proj) %>% as.data.frame()
-
+infectantes$CASOS 
 
 
 #################################################
 #Estimativa do número de recuperados
 #################################################
-#Considerou-se recuperado o indivíduo com mais de 8 dias de início dos sintomas e que não foi a óbito. Óbitos e recuperados
+#Considerou-se recuperado o indivíduo com mais de 14 dias após a contaminação e que não foi a óbito. Óbitos e recuperados
 #são cumulativos
-recuperados <- covid
-recuperados$DATA <- (recuperados$INICIO_SINTOMAS + 9) %>% as.character()
-recuperados$INICIO_SINTOMAS <- NULL
+recuperados <- casos_arima
+recuperados$DATA <- (recuperados$DATA+14) %>% as.character()
 names(recuperados)[1] <- "RECUPERADOS"
 recuperados$DATA <- as.Date(recuperados$DATA)
+
 recuperados <- merge(recuperados, obitos, by = "DATA", all = T)
 recuperados[is.na(recuperados)] <- 0
 recuperados$RECUPERADOS <- recuperados$RECUPERADOS - recuperados$OBITOS
@@ -195,7 +211,7 @@ base$CUM_OBITOS <- cumsum(base$OBITOS)
 #################################################
 #Estimativa do número de suscetíveis
 #################################################
-POP <- 500973
+POP <- 7164788
 base$SUSCETIVEIS <- POP - base$CUM_RECUPERADOS - base$CUM_OBITOS - base$EXPOSTOS - base$INFECTANTES
 
 #################################################
@@ -203,36 +219,22 @@ base$SUSCETIVEIS <- POP - base$CUM_RECUPERADOS - base$CUM_OBITOS - base$EXPOSTOS
 #################################################
 #merge dos dados de ocupação de leitos e número de intenação com os outros dados
 base$DATA <- as.Date(base$DATA)
-leitos_uti$DATA <- as.Date(leitos_uti$DATA) 
-base <- merge(base, leitos_uti, by = "DATA", all = T)
-inter_recup$DT_ENTUTI <- as.Date(inter_recup$DT_ENTUTI)
-base <- merge(base, inter_recup, by.x = "DATA", by.y = "DT_ENTUTI", all = T)
-base <- subset(base, base$DATA < Sys.Date())
-
-base[is.na(base$LEITOS_UTI), names(base) == "LEITOS_UTI"] <- 0
-base[is.na(base$INTERNACAO_UTI), names(base) == "INTERNACAO_UTI"] <- 0
-base$CUM_INTERNACAO_UTI <- cumsum(base$INTERNACAO_UTI)
-base[is.na(base$RECUPERADOS_UTI), names(base) == "RECUPERADOS_UTI"] <- 0
-base$CUM_RECUPERADOS_UTI <- cumsum(base$RECUPERADOS_UTI)
-base <- na.omit(base)
 base <- unique(base)
-
-ggplot(base, aes(DATA, LEITOS_UTI))+
-	geom_line()
+base <- na.omit(base)
 
 # Estimando o Rt ----------------------------------------------------------
 source("apeEstim.R")
 source("apePredPost.R")
 
-incidencia <- casos_arima
-names(incidencia) <- c('CASOS_NOVOS','DATA')
+incidencia <- base
+incidencia <- incidencia %>% dplyr::select('CASOS', 'DATA')
 incidencia <- subset(incidencia, incidencia$DATA > c(Sys.Date()-92)) #Utilizando dados dos últimos trës meses
 
 #Left trunc
 trunc <- 0
 
-Icovid <- incidencia$CASOS_NOVOS #Incidência
-gencovid <- EpiEstim::discr_si(c(0:max(incidencia$CASOS_NOVOS)), mu = 4.8, sigma = 2.3) #distribuição gama
+Icovid <- incidencia$CASOS #Incidência
+gencovid <- EpiEstim::discr_si(c(0:max(incidencia$CASOS)), mu = 4.8, sigma = 2.3) #distribuição gama
 Lcovid <- overall_infectivity(Icovid, gencovid)
 
 #Priors and settings
@@ -267,63 +269,32 @@ ggplot(res_melt, aes(DATA, value, group = variable, color = variable))+
 
 # Forecast do número de casos e dos óbitos --------------------------------
 #Estados Iniciais
-#Estados iniciais
-##Suscetiveis = s.num = População - Expostos - Infectados - Recuperados - Óbitos
-##Expostos (não transmissores) = e.num = Estimado do nowcasting
-##Infectados (transmissores) = i.num = Estimado do nowcasting
-##Recuperados = r.num = Estimado do nowcasting
-##Óbitos = d.num = Estimado do nowcasting
-#Parâmetros
-##Número de reprodução efetivo = Rt = Estimado do nowcasting
-##Duração da exposição (não transmissível) = e.dur = 2 dias
-##Duração da infecção (trainsmissível) = i.dur = 12 dias (3 antes do início dos sintomas, 1 do início dos sintomas e 8 após o início dos sintomas)
-#prob1 = Taxa de internação
-##prob2 = Taxa de letalidade entre os internados
-tempos <- read_csv("nowcasting/dados/tempos.csv")
-uti.dur.mean <- tempos$uti.dur.mean
-tempo_infectividade_uti <- tempos$tempo_infectividade_uti
-tempo_uti_obito <- tempos$tempo_uti_obito
-tempo_uti_recuperacao <- tempos$tempo_uti_recuperacao
-
 S <- tail(base$SUSCETIVEIS,1)[1]
 E <- tail(base$EXPOSTOS,1)[1]
 I <- tail(base$INFECTANTES,1)[1]
-H <- tail(base$LEITOS_UTI,1)[1]
 R <- tail(base$CUM_RECUPERADOS,1)[1]
 D <- tail(base$CUM_OBITOS,1)[1]
-N <- S + E + I +H +  R + D
-ir.dur <- 12
+N <- S + E + I +  R + D
+id.dur <- 12
 ei.dur <- 2
-ih.dur <- tempo_infectividade_uti
-hd.dur <- tempo_uti_obito
-hr.dur <- tempo_uti_recuperacao
 etha <- 1/ir.dur
 betha <- 1/ei.dur
-delta <- 1/ih.dur
-mu <- 1/hd.dur
-epsilon <- 1/hr.dur
+delta <- 1/id.dur
 
 #Estimativa das probabilidade para o modelo
 base$TOTAL <- base$EXPOSTOS + base$INFECTANTES + base$CUM_RECUPERADOS + base$CUM_OBITOS
-base$CUM_CASOS_NOVOS <- cumsum(base$CASOS_NOVOS)
-defasagem_uti_recuperacao <- nrow(base) - (ei.dur + ih.dur + hr.dur) #linha da base ajustada pelo Lag de tempo (ei.dur + ih.dur + hr.dur) para calcular a probabilidade da pessoa entrar na UTI e se recuperaram
-defasagem_uti_obito <- nrow(base) - (ei.dur + ih.dur + hd.dur) #linha da base ajustada pelo Lag de tempo (ei.dur + ih.dur + hd.dur) para calcular a probabilidade da pessoa entrar na UTI e falecer
-prob1 <- tail(base$CUM_RECUPERADOS_UTI,1)[1]/base[,names(base) == "TOTAL"][defasagem_uti_recuperacao] #Taxa de hospitalizados recuperados de UTI - denominador usando a defasagem
-prob2 <- tail(base$CUM_OBITOS,1)[1]/base[,names(base) == "TOTAL"][defasagem_uti_obito] #Taxa de letalidade entre os internados em UTI - denominador usando a defasagem
-prob <- prob1 + prob2
+base$CUM_CASOS <- cumsum(base$CASOS)
+prob1 <- tail(base$CUM_OBITOS,1)/tail(base$TOTAL,1) #Taxa de hospitalizados recuperados de UTI - denominador usando a defasagem
 
 init <- init.dcm(S = S,
 		 E = E,
 		 I = I,
-		 H = H,
 		 R = R,
 		 D = D,
 		 se.flow = 0,
 		 ei.flow = 0,
 		 ir.flow = 0,
-		 ih.flow = 0,
-		 hr.flow = 0,
-		 hd.flow = 0
+		 id.flow = 0
 )
 
 
@@ -333,45 +304,37 @@ param <- param.dcm(Rt = c(tail(res_base$IC025,1),
 			  tail(res_base$IC975,1)),
 		   etha = 1/ir.dur,
 		   betha = 1/ei.dur,
-		   delta = 1/ih.dur,
-		   mu = 1/hd.dur,
-		   epsilon = 1/hr.dur,
-		   prob1 = prob1,
-		   prob2 = prob2,
-		   prob = prob
+		   delta = 1/id.dur,
+		   prob1 = prob1
 )
 
 
 #Função SEIRD
-SEIHRD <- function(t, t0, parms) {
+SEIRD <- function(t, t0, parms) {
 	with(as.list(c(t0, parms)), {
 		
-		N <- S + E + I + H +  R + D
+		N <- S + E + I +  R + D
 		
 		alpha <- etha * Rt * I/N
 		
 		#Equações diferenciais
 		dS <- -alpha*S
 		dE <- alpha*S - betha*E
-		dI <- betha*E - prob*delta*I - (1-prob)*etha*I
-		dH <- prob*delta*I - (prob2/prob)*mu*H - (prob1/prob)*epsilon*H
-		dR <- (1 - prob)*etha*I + (prob1/prob)*epsilon*H
-		dD <- (prob2/prob)*mu*H
+		dI <- betha*E - prob1*delta*I - (1-prob1)*etha*I
+		dR <- (1 - prob1)*etha*I 
+		dD <- prob1*delta*I
 		
 		#Outputs
-		list(c(dS, dE, dI, dH, dR, dD,
+		list(c(dS, dE, dI, dR, dD,
 		       se.flow = alpha * S,
 		       ei.flow = betha * E,
-		       ir.flow = (1 - prob)*etha*I,
-		       ih.flow = prob1*delta*I,
-		       hr.flow = (prob1/prob)*epsilon*H,
-		       hd.flow = (prob2/prob)*mu*H),
+		       ir.flow = (1 - prob1)*etha*I,
+		       id.flow = prob1*delta*I),
 		     num = N,
 		     s.prev = S / N,
 		     e.prev = E / N,
 		     i.prev = I / N,
 		     ei.prev = (E + I)/N,
-		     h.prev = H /N,
 		     r.prev = R / N,
 		     d.prev = D / N)
 	})
@@ -380,7 +343,7 @@ SEIHRD <- function(t, t0, parms) {
 
 #Resolvendo as equações diferenciais
 projecao <- 21
-control <- control.dcm(nsteps = projecao, new.mod = SEIHRD)
+control <- control.dcm(nsteps = projecao, new.mod = SEIRD)
 mod <- dcm(param, init, control)
 
 
@@ -391,7 +354,6 @@ mod <- dcm(param, init, control)
 resultados_cenario_1 <- data.frame(SUSCETIVEIS = mod$epi$S$run1,
 				   EXPOSTOS = mod$epi$E$run1,
 				   INFECTANTES = mod$epi$I$run1,
-				   LEITOS_UTI = mod$epi$H$run1,
 				   CUM_RECUPERADOS = mod$epi$R$run1,
 				   CUM_OBITOS = mod$epi$D$run1
 )
@@ -399,9 +361,9 @@ resultados_cenario_1 <- data.frame(SUSCETIVEIS = mod$epi$S$run1,
 resultados_cenario_1 <- resultados_cenario_1[-1,]
 resultados_cenario_1$DATA <- c((Sys.Date()):(Sys.Date()+projecao-2))
 resultados_cenario_1$DATA  <- as.Date(resultados_cenario_1$DATA , origin = "1970-01-01")
-base_select <- base %>% dplyr::select(DATA,SUSCETIVEIS, CUM_RECUPERADOS, EXPOSTOS, INFECTANTES, CUM_OBITOS, LEITOS_UTI)
+base_select <- base %>% dplyr::select(DATA,SUSCETIVEIS, CUM_RECUPERADOS, EXPOSTOS, INFECTANTES, CUM_OBITOS)
 resultados_cenario_1 <- rbind(base_select, resultados_cenario_1)
-names(resultados_cenario_1) <-c("DATA", "SUSCETIVEIS_CENARIO_1", "CUM_RECUPERADOS_CENARIO_1", "EXPOSTOS_CENARIO_1", "INFECTANTES_CENARIO_1", "CUM_OBITOS_CENARIO_1", "LEITOS_UTI_CENARIO_1")
+names(resultados_cenario_1) <-c("DATA", "SUSCETIVEIS_CENARIO_1", "CUM_RECUPERADOS_CENARIO_1", "EXPOSTOS_CENARIO_1", "INFECTANTES_CENARIO_1", "CUM_OBITOS_CENARIO_1")
 
 ######################################
 #Cenário 2 - Rt Mediana
@@ -409,16 +371,15 @@ names(resultados_cenario_1) <-c("DATA", "SUSCETIVEIS_CENARIO_1", "CUM_RECUPERADO
 resultados_cenario_2 <- data.frame(SUSCETIVEIS = mod$epi$S$run2,
 				   EXPOSTOS = mod$epi$E$run2,
 				   INFECTANTES = mod$epi$I$run2,
-				   LEITOS_UTI = mod$epi$H$run2,
 				   CUM_RECUPERADOS = mod$epi$R$run2,
 				   CUM_OBITOS = mod$epi$D$run2)
 
 resultados_cenario_2 <- resultados_cenario_2[-1,]
 resultados_cenario_2$DATA <- c((Sys.Date()):(Sys.Date()+projecao-2))
 resultados_cenario_2$DATA  <- as.Date(resultados_cenario_2$DATA , origin = "1970-01-01")
-base_select <- base %>% dplyr::select(DATA,SUSCETIVEIS, CUM_RECUPERADOS, EXPOSTOS, INFECTANTES, CUM_OBITOS, LEITOS_UTI)
+base_select <- base %>% dplyr::select(DATA,SUSCETIVEIS, CUM_RECUPERADOS, EXPOSTOS, INFECTANTES, CUM_OBITOS)
 resultados_cenario_2 <- rbind(base_select, resultados_cenario_2)
-names(resultados_cenario_2) <-c("DATA", "SUSCETIVEIS_CENARIO_2", "CUM_RECUPERADOS_CENARIO_2", "EXPOSTOS_CENARIO_2", "INFECTANTES_CENARIO_2", "CUM_OBITOS_CENARIO_2", "LEITOS_UTI_CENARIO_2")
+names(resultados_cenario_2) <-c("DATA", "SUSCETIVEIS_CENARIO_2", "CUM_RECUPERADOS_CENARIO_2", "EXPOSTOS_CENARIO_2", "INFECTANTES_CENARIO_2", "CUM_OBITOS_CENARIO_2")
 
 ######################################
 #Cenário 3 - Rt IC975
@@ -426,30 +387,26 @@ names(resultados_cenario_2) <-c("DATA", "SUSCETIVEIS_CENARIO_2", "CUM_RECUPERADO
 resultados_cenario_3 <- data.frame(SUSCETIVEIS = mod$epi$S$run3,
 				   EXPOSTOS = mod$epi$E$run3,
 				   INFECTANTES = mod$epi$I$run3,
-				   LEITOS_UTI = mod$epi$H$run3,
 				   CUM_RECUPERADOS = mod$epi$R$run3,
 				   CUM_OBITOS = mod$epi$D$run3)
 
 resultados_cenario_3 <- resultados_cenario_3[-1,]
 resultados_cenario_3$DATA <- c((Sys.Date()):(Sys.Date()+projecao-2))
 resultados_cenario_3$DATA  <- as.Date(resultados_cenario_3$DATA , origin = "1970-01-01")
-base_select <- base %>% dplyr::select(DATA,SUSCETIVEIS, CUM_RECUPERADOS, EXPOSTOS, INFECTANTES, CUM_OBITOS, LEITOS_UTI)
+base_select <- base %>% dplyr::select(DATA,SUSCETIVEIS, CUM_RECUPERADOS, EXPOSTOS, INFECTANTES, CUM_OBITOS)
 resultados_cenario_3 <- rbind(base_select, resultados_cenario_3)
-names(resultados_cenario_3) <-c("DATA", "SUSCETIVEIS_CENARIO_3", "CUM_RECUPERADOS_CENARIO_3", "EXPOSTOS_CENARIO_3", "INFECTANTES_CENARIO_3", "CUM_OBITOS_CENARIO_3", "LEITOS_UTI_CENARIO_3")
+names(resultados_cenario_3) <-c("DATA", "SUSCETIVEIS_CENARIO_3", "CUM_RECUPERADOS_CENARIO_3", "EXPOSTOS_CENARIO_3", "INFECTANTES_CENARIO_3", "CUM_OBITOS_CENARIO_3")
 
 #Unindo as bases de resultados
 resultados <- merge(resultados_cenario_1, resultados_cenario_2, by= "DATA", all = T)
 resultados <- merge(resultados_cenario_3, resultados, by= "DATA", all = T)
 
 
-write.csv(resultados, "nowcasting/dados/resultados.csv", row.names = F)
+write.csv(resultados, "dados/resultados.csv", row.names = F)
 #resultados$DATA <- as.Date(resultados$DATA, origin = "1970-01-01")
-write_sheet(id_covid,"resultados", data = resultados)
-
 
 #Análise dos resultados
-
-resultados_melt <- resultados %>% dplyr::select(DATA, CUM_OBITOS_CENARIO_3, LEITOS_UTI_CENARIO_3)
+resultados_melt <- resultados %>% dplyr::select(DATA, CUM_OBITOS_CENARIO_3)
 resultados_melt <- melt(resultados_melt, id.vars = "DATA")
 
 ggplot(resultados_melt, aes(DATA, value, color = variable))+
